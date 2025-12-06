adc48e68e9f0e11154c02d52c3e718f9d057e527
Stevie McCann <stevie.mccann@live.co.uk>
2025-12-06 21:55:58 +0000

    chore: enhance linting setup with eslint-plugin-prettier and add lint:fix scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  "*.{json,md}": ["prettier --write"],
   "package.json": ["sort-package-json"],
 };
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "es5",
+  printWidth: 80,
+};
+
+export default config;
+

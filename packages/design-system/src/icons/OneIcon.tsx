c02a62723358931e6a993556345a4573be51cbad
Stevie McCann <stevie.mccann@live.co.uk>
2025-12-08 13:38:54 +0000

    feat(design-system): add OneIcon and TwoIcon

---
+++ packages/design-system/src/icons/OneIcon.tsx
@@ -0,0 +1,16 @@
+export const OneIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={1.5}
+  >
+    <title>One</title>
+    <path d="m12 16 0 -8v0.66667C12 9.40304 11.403 10 10.6667 10L10 10" />
+    <path d="M14 16h-4" />
+    <path d="M0.75 12a11.25 11.25 0 1 0 22.5 0 11.25 11.25 0 1 0 -22.5 0" />
+  </svg>
+);
